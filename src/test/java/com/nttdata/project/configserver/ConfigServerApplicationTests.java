23a309061cc6fb83fe6edbad32e114d4be8e3eea
edinson-boada <edinson.boada@unmsm.edu.pe>
2022-05-03 10:55:35 -0500

    rename packages

--- src/test/java/com/nttdata/project/configserver/ConfigServerApplicationTests.java
+++ src/test/java/com/nttdata/project/configserver/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nttdata.project01.configserver;
+package com.nttdata.project.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
